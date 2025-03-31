8f01485840ea30a8ab893f4fe72e2eb9fd71aecc
RamazanImashov <ramazanimashov.dev@gmail.com>
2025-03-31 15:07:29 +0600

    chanch

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 pages = {
     "Pages": [
-        st.Page("pages/pull_users.py", title="Пользователи"),
+        st.Page("pages/pull_users.py", title="Пользователи hor-log"),
+        st.Page("pages/dordoi_pull_users.py", title="Пользователи Дордой"),
         st.Page("pages/send_message.py", title="Отправить сообщение"),
         st.Page("pages/stats.py", title="Статистика"),
         st.Page("pages/barcode_scanner.py", title="Сканер штрих кода"),
