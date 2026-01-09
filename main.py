b71a5e60051169745568a426a6e895b8748ae03b
RamazanImashov <ramazanimashov.dev@gmail.com>
2026-01-09 19:21:48 +0600

    add new pages

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 pages = {
     "Pages": [
-        st.Page("pages/pull_users.py", title="Пользователи hor-log"),
+        st.Page("pages/horizon_pull_users.py", title="Пользователи hor-log"),
         st.Page("pages/dordoi_pull_users.py", title="Пользователи Дордой"),
         st.Page("pages/send_message.py", title="Отправить сообщение"),
         st.Page("pages/stats.py", title="Статистика"),
+        st.Page("pages/download_users.py", title="Загрузка данных"),
         st.Page("pages/barcode_scanner.py", title="Сканер штрих кода"),
     ],
 }
---
+++ utils/to_excel.py
@@ -0,0 +1,6 @@
+
+def to_excel(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Clients')
+    return output.getvalue()
