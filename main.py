a27457d4a9e995cb095ff2dc9615688cda230380
RamazanImashov <ramazanimashov.dev@gmail.com>
2026-01-23 17:08:37 +0600

    fix

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ pages = {
     "Pages": [
         st.Page("pages/horizon_pull_users.py", title="Пользователи hor-log"),
         st.Page("pages/dordoi_pull_users.py", title="Пользователи Дордой"),
-        st.Page("pages/send_message.py", title="Отправить сообщение"),
+        # st.Page("pages/send_message.py", title="Отправить сообщение"),
         st.Page("pages/stats.py", title="Статистика"),
-        st.Page("pages/download_users.py", title="Загрузка данных"),
-        st.Page("pages/barcode_scanner.py", title="Сканер штрих кода"),
+        st.Page("pages/download_users_file.py", title="Загрузка данных"),
+        st.Page("pages/dcodegenerator.py", title="Создание D код изображений"),
+        st.Page("pages/fcodegenerator.py", title="Создание F код изображений"),
+        # st.Page("pages/barcode_scanner.py", title="Сканер штрих кода"),
     ],
 }
 
