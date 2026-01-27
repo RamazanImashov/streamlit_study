b7e4d0bf7353ede0729501c6122d67d1ba491bc1
RamazanImashov <ramazanimashov.dev@gmail.com>
2026-01-27 12:20:48 +0600

    delete horizon files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 pages = {
     "Страницы": [
-        st.Page("pages/dordoi_pull_users.py", title="Пользователи Дордой"),
+        st.Page("pages/dordoi_pull_users.py", title="Клиенты Дордой"),
         st.Page("pages/download_users_file.py", title="Загрузка данных"),
         st.Page("pages/fcodegenerator.py", title="Создание F код изображений"),
     ],
