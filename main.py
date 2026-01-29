2936619685f65ada1ff56b58b20dde1b4c4d9be5
RamazanImashov <ramazanimashov.dev@gmail.com>
2026-01-29 12:38:46 +0600

    туц ашчруы

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pages = {
         st.Page("pages/stats.py", title="Статистика"),
         st.Page("pages/download_users_file.py", title="Загрузка данных клиентов"),
         st.Page("pages/dcodegenerator.py", title="Создание D код изображений"),
-        st.Page("pages/01_free_d_codes.py"),
+        st.Page("pages/01_free_d_codes.py", title="Свободные D коды"),
     ],
 }
 
