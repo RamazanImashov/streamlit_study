df559dc242eb8f2115381b230d18126c915b0f53
RamazanImashov <ramazanimashov.dev@gmail.com>
2026-01-29 13:11:23 +0600

    dordou

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pages = {
         st.Page("pages/dordoi_pull_users.py", title="Клиенты Дордой"),
         st.Page("pages/download_users_file.py", title="Загрузка данных"),
         st.Page("pages/fcodegenerator.py", title="Создание F код изображений"),
+        st.Page("pages/01_free_f_codes.py", title="Свободные F коды"),
     ],
 }
 
